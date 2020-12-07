599bae6426673daa6c0acbe57598fd8dcf756277
janojansen <62889517+janojansen@users.noreply.github.com>
2020-12-07 19:36:13 +0100

    Update manualcontroller.py

--- ROS_Package/rdd_movingspeaker/scripts/manualcontroller.py
+++ ROS_Package/rdd_movingspeaker/scripts/manualcontroller.py
@@ -3,11 +3,18 @@
 import rospy
 from geometry_msgs.msg import Twist
 from rdd_movingspeaker.msg import manualcontrol, midiconfig
+#midiConfig = is set to true if MIDI-Config is available and in right format
 midiConfig=False
+#manualcontrol_publisher = global handle for publishing msgs to cmd_vel
 manualcontrol_publisher=0
+#control = dict that describes the current cmd_vel of the robot
+# [[x,y,z][α,β,γ]]
 control=[[0,0,0],[0,0,0]]
+#CHANNEL_ROBOT = MIDI-CHANNEL over which robot is communicating to the GUI
 CHANNEL_ROBOT=rospy.get_param("channel")
+#prescaler = used to calculate the maximum time for the alive signal
 prescaler=50
+#alive = robot is alive until alive equals the value of the prescaler
 alive=prescaler+1
 
 def callback_midimanual(data):
@@ -18,13 +25,14 @@ def callback_midimanual(data):
     try:
         if midiConfig:
             alive=1
-            if (data.mode == 7): # alive signal
+            if (data.mode == 7): # mode for alive signal
                 write_pub_vel_msg(control)
                 return
             if (data.channel != CHANNEL_ROBOT):
                 return
+            #calls movement function that corresponds to the given pitch of the current data
             options[(data.pitch)](data)
-            write_pub_vel_msg(control)###
+            write_pub_vel_msg(control)
             print("got befehl")
         if not midiConfig:
             print("no midiconfig!!")
@@ -36,7 +44,9 @@ def callback_instructions(data):
     """Gets instructions from jsonreceiver and fills them into dataconstruct"""
     global midiConfig
     global options
+    #setting variable midiConfig to true --> commands can now be sent
     midiConfig=True
+    #load functions in dict options with the corresponding keys
     options={data.move_forward: move_forward,
              data.move_backward: move_backward,
              data.strafe_left: strafe_left,
@@ -87,9 +97,17 @@ def write_pub_vel_msg(control):
     
 def calculate_speed(data, i1, i2, direction):
     """Calculates the speed and direction"""
+    #i1 = 0 for linear speed
+    #i1 = 1 for angular speed
+    #i2 = 0,1,2 for x,y,z (linear) or α,β,γ (angular)
+    #direction = +1 if moving forward
+    #direction = -1 if moving backward
     global control
+    #mode = 1 equals to note on msg (MIDI) --> speed is calculated from velocity (MIDI)
+    #max speed is 1.0
     if (data.mode == 1):
         speed = (direction/127.0)*data.velocity
+    #mode = 0 equals to note off msg (MIDI) --> speed is set to zero
     if (data.mode == 0):
         speed = 0
     control[i1][i2] = speed
